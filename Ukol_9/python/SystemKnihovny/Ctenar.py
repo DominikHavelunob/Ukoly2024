03b577fbe97f7d4f3542a486161f2970def0908c
DominikHavelunob <dominik.havel@unob.cz>
2025-03-11 16:19:33 +0100

    Update Ctenar.py

--- Ukol_9/python/SystemKnihovny/Ctenar.py
+++ Ukol_9/python/SystemKnihovny/Ctenar.py
@@ -1,17 +1,40 @@
 import random
 
-
 class Ctenar:
     def __init__(self, jmeno: str, prijmeni: str):
         self._jmeno = jmeno
         self._prijmeni = prijmeni
-        self.cislo_prukazky = self.vygeneruj_cislo_prukazky()
+        self._cislo_prukazky = self.vygeneruj_cislo_prukazky()
 
-    # doplňte potřebné gettry a settry
+    @property
+    def jmeno(self) -> str:
+        return self._jmeno
+
+    @jmeno.setter
+    def jmeno(self, value: str):
+        self._jmeno = value
+
+    @property
+    def prijmeni(self) -> str:
+        return self._prijmeni
+
+    @prijmeni.setter
+    def prijmeni(self, value: str):
+        self._prijmeni = value
+
+    @property
+    def cislo_prukazky(self) -> int:
+        return self._cislo_prukazky
+
+    @cislo_prukazky.setter
+    def cislo_prukazky(self, value: int):
+        if value <= 0:
+            raise ValueError("Číslo průkazky musí být kladné celé číslo.")
+        self._cislo_prukazky = value
 
     @staticmethod
     def vygeneruj_cislo_prukazky() -> int:
-        pass
+        return random.randint(1, 10000)
 
     def __str__(self) -> str:
-        return ""
+        return f"Čtenář: {self._jmeno} {self._prijmeni}, Číslo průkazky: {self._cislo_prukazky}"
